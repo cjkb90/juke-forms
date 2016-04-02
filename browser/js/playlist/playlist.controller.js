1abcc427cb68ff78db499889a43fe1a2f9b140f9
Christopher Kennedy <cjkb90@users.noreply.github.com>
2016-04-02 16:07:10 -0400

    Starting 3.5

--- browser/js/playlist/playlist.controller.js
+++ browser/js/playlist/playlist.controller.js
@@ -1,3 +1,8 @@
 juke.controller('PlaylistCtrl',function($scope){
-
+	$scope.createPlaylist = function(){
+		console.log('clicked');
+	}
+	$scope.log = function(){
+		console.log($scope.playlistForm);
+	}
 });
